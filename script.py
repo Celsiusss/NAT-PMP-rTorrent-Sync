d6671841308a7c3c7882177f1c4ac28c4e251734
Celsiuss <celsiuss@await.sh>
2023-09-10 02:45:01 +0200

    environment variable support

--- script.py
+++ script.py
@@ -4,13 +4,20 @@ import xmlrpc.client
 import natpmp
 import sys
 import schedule
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RPC_URL=os.getenv("RPC_URL") or "http://localhost/RPC2"
+GATEWAY=os.getenv("GATEWAY") or "10.2.0.1"
 
 def log(msg, err = False):
     date = datetime.datetime.now()
     print(f"[{date}] {msg}", file=sys.stderr if err else sys.stdout)
 
 def set_port(port):
-    with xmlrpc.client.ServerProxy("http://192.168.1.100:1313/RPC2") as s:
+    with xmlrpc.client.ServerProxy(RPC_URL) as s:
         s.network.port_range.set("", f"{port}-{port}")
 
 def run():
@@ -22,7 +29,7 @@ def run():
     except:
         previous_port = 0
 
-    res = natpmp.map_port(natpmp.NATPMP_PROTOCOL_TCP, 0, 0, 60, "10.2.0.1")
+    res = natpmp.map_port(natpmp.NATPMP_PROTOCOL_TCP, 0, 0, 60, GATEWAY)
 
     if res.result != 0:
         log("Error: " + res, True)
@@ -33,6 +40,7 @@ def run():
                 f.write(str(res.public_port))
             log("Assigned new port: " + str(res.public_port))
 
+run()
 schedule.every(30).seconds.do(run)
 
 while True:
